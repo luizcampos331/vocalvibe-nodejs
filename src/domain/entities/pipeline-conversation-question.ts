4dc481efea19a5445c0edc916183f14cc329755e
Luiz Campos <luizcampos331@gmail.com>
2024-12-15 19:40:50 -0300

    feat: add answered column in pipeline conversation question entity

--- src/domain/entities/pipeline-conversation-question.ts
+++ src/domain/entities/pipeline-conversation-question.ts
@@ -4,6 +4,7 @@ export type PipelineConversationQuestionProps = EntityProps & {
   pipelineConversationId: string;
   question: string;
   filename: string;
+  answered?: boolean;
 };
 
 export type PipelineConversationQuestionJSON =
@@ -13,12 +14,14 @@ class PipelineConversationQuestion extends Entity {
   private readonly _pipelineConversationId: string;
   private readonly _question: string;
   private readonly _filename: string;
+  private readonly _answered: boolean;
 
   constructor(props: PipelineConversationQuestionProps) {
     super(props);
     this._pipelineConversationId = props.pipelineConversationId;
     this._question = props.question;
     this._filename = props.filename;
+    this._answered = props?.answered || false;
   }
 
   public static create(
@@ -33,6 +36,7 @@ class PipelineConversationQuestion extends Entity {
       pipelineConversationId: this._pipelineConversationId,
       question: this._question,
       filename: this._filename,
+      answered: this._answered,
       createdAt: this._createdAt,
       updatedAt: this._updatedAt,
       deletedAt: this._deletedAt,
